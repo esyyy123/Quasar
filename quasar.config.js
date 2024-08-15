85c9cb3e6994a75beb8aa060124a7038a52d776d
SATNUSA\sakti.prawira <sakti.prawira@satnusa.com>
2024-08-15 09:03:21 +0700

    for development

--- quasar.config.js
+++ quasar.config.js
@@ -79,6 +79,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       https: false,
+      port: 44554,
       open: false, // opens browser window automatically
       host: "localhost",
       server: {
